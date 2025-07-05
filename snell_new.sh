7b30747cf053c81fa47883ee7db60e3016733648
gdyan <120347640+gdyan2022@users.noreply.github.com>
2025-07-05 10:52:13 +0800

    Update snell_new.sh

--- snell_new.sh
+++ snell_new.sh
@@ -10,7 +10,7 @@ export PATH
 #=================================================
 
 sh_ver="1.5.0_gdyan"
-surge_ver="4.1.1"
+surge_ver="5.0.0b1"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file_1=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 FOLDER="/etc/snell/"
